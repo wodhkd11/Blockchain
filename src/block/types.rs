bea58b1a8c8e17ffe136911017c627cd4c9489de
JWCHOI03 <jaewang_choi@gm.gist.ac.kr>
2026-01-25 17:56:53 +0900

    learning

--- src/block/types.rs
+++ src/block/types.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use primitive_types::H256;
+use rlp::{Decodable, Encodable};
 use serde::{Deserialize, Serialize};
 use serde_big_array::BigArray;
 
@@ -73,3 +75,44 @@ pub struct TransactionForDB{
 pub type Balance = primitive_types::U256;
 pub type Nonce = u64;
 
+#[derive(Debug, Clone)]
+pub struct PrimaryAsset{
+    pub ticker: TokenTicker,
+    pub amount: Balance,
+}
+
+pub struct AccountState{
+    pub nonce: u64,
+    pub primary_assets: Vec<PrimaryAsset>,
+    pub asset_root: H256,
+}
+
+impl Encodable for PrimaryAsset{
+    fn rlp_append(&self, s: &mut rlp::RlpStream) {
+        s.begin_list(2);
+        s.append(&self.ticker);
+        s.append(&self.amount);
+    }
+}
+impl Decodable for PrimaryAsset{
+    fn decode(rlp: &rlp::Rlp) -> Result<Self, rlp::DecoderError> {
+        Ok(Self{
+            ticker: rlp.val_at(0)?,
+            amount: rlp.val_at(1)?,
+        })
+    }
+}
+
+impl Encodable for AccountState{
+    fn rlp_append(&self, s: &mut rlp::RlpStream) {
+        s.begin_list(3);
+        s.append(&self.nonce);
+        s.append_list(&self.primary_assets);
+        s.append(&self.asset_root);
+    }
+}
+impl Decodable for AccountState{
+    fn decode(rlp: &rlp::Rlp) -> Result<Self, rlp::DecoderError> {
+        Ok(Self { nonce: rlp.val_at(0)?, primary_assets: rlp.list_at(1)?, asset_root: rlp.val_at(2)? })
+    }
+}
